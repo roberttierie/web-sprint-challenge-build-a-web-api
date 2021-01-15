dfa9930fbdf534b6ee4f91122fa603abeb409d36
Robert Tierie <tangodolphin13@gmail.com>
2021-01-15 18:11:43 +0100

    mvp compelte

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require('./api/server')
+const port = 6000
+
+server.listen(port,()=>{
+    console.log(`The server is up at https://localhost:${port}`)
+})
